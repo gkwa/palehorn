0dafef59bbd714fda8bb2fb13635992cb5c0a761
Taylor Monacelli <taylormonacelli@gmail.com>
2023-05-15 11:24:13 -0700

    [skip ci] this should work on a machine somewhere

--- cmd/test.go
+++ cmd/test.go
@@ -53,25 +53,6 @@ func init() {
 
 // simple does nothing except block while running the service.
 
-var logger service.Logger
-
-type program struct{}
-
-func (p *program) Start(s service.Service) error {
-	// Start should not block. Do the actual work async.
-	go p.run()
-	return nil
-}
-
-func (p *program) run() {
-	// Do work here
-}
-
-func (p *program) Stop(s service.Service) error {
-	// Stop should not block. Return with a few seconds.
-	return nil
-}
-
 func test() {
 	svcConfig := &service.Config{
 		Name:        "GoServiceExampleSimple",
